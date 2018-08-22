0347ce0e37d97b3b35e474b5ab3947ab47239a7a
xuxianhong <xuxianhong91@gmail.com>
2018-08-22 11:05:20 +0800

    first commit main method ok

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "doudou"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies += "org.bytedeco" % "javacv-platform" % "1.4.2"
+
+        
---
+++ src/main/java/CameraCV.java
@@ -0,0 +1,93 @@
+/**
+ * Created by xxh on 18-8-21.
+ */
+
+import javax.swing.JFrame;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.avcodec;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+
+
+public class CameraCV {
+
+    public static void main(String[] args) throws Exception, InterruptedException, FrameRecorder.Exception {
+        recordCamera("rtmp://qn.live-send.acg.tv/live-qn/?streamname=live_40151376_7027683&key=f08ece9aa5d9f489fa0a8b1058659c4e",25);
+    }
+
+
+    /**
+     * 按帧录制本机摄像头视频（边预览边录制，停止预览即停止录制）
+     *
+     * @author eguid
+     * @param outputFile -录制的文件路径，也可以是rtsp或者rtmp等流媒体服务器发布地址
+     * @param frameRate - 视频帧率
+     * @throws Exception
+     * @throws InterruptedException
+     * @throws org.bytedeco.javacv.FrameRecorder.Exception
+     */
+    public static void recordCamera(String outputFile, double frameRate)
+            throws Exception, InterruptedException, org.bytedeco.javacv.FrameRecorder.Exception {
+        Loader.load(opencv_objdetect.class);
+        FrameGrabber grabber = FrameGrabber.createDefault(0);//本机摄像头默认0，这里使用javacv的抓取器，至于使用的是ffmpeg还是opencv，请自行查看源码
+        grabber.start();//开启抓取器
+
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();//转换器
+        opencv_core.IplImage grabbedImage = converter.convert(grabber.grab());//抓取一帧视频并将其转换为图像，至于用这个图像用来做什么？加水印，人脸识别等等自行添加
+        int width = grabbedImage.width();
+        int height = grabbedImage.height();
+
+        FrameRecorder recorder = FrameRecorder.createDefault(outputFile, width, height);
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264); // avcodec.AV_CODEC_ID_H264，编码
+        recorder.setFormat("flv");//封装格式，如果是推送到rtmp就必须是flv封装格式
+        recorder.setFrameRate(frameRate);
+
+        recorder.start();//开启录制器
+        long startTime=0;
+        long videoTS=0;
+        CanvasFrame frame = new CanvasFrame("camera", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(true);
+        Frame rotatedFrame=converter.convert(grabbedImage);//不知道为什么这里不做转换就不能推到rtmp
+        while (frame.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
+            rotatedFrame = converter.convert(grabbedImage);
+            frame.showImage(rotatedFrame);
+            if (startTime == 0) {
+                startTime = System.currentTimeMillis();
+            }
+            videoTS = 1000 * (System.currentTimeMillis() - startTime);
+            recorder.setTimestamp(videoTS);
+            recorder.record(rotatedFrame);
+            Thread.sleep(40);
+        }
+        frame.dispose();
+        recorder.stop();
+        recorder.release();
+        grabber.stop();
+
+    }
+
+
+    public static void openCamera() throws Exception, InterruptedException {
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        grabber.start();   //开始获取摄像头数据
+        CanvasFrame canvas = new CanvasFrame("摄像头");//新建一个窗口
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        canvas.setAlwaysOnTop(true);
+
+        while (true) {
+            if (!canvas.isDisplayable()) {//窗口是否关闭
+                grabber.stop();//停止抓取
+                System.exit(2);//退出
+            }
+            canvas.showImage(grabber.grab());//获取摄像头图像并放到窗口上显示， 这里的Frame frame=grabber.grab(); frame是一帧视频图像
+
+            Thread.sleep(50);//50毫秒刷新一次图像
+        }
+    }
+
+}
+
